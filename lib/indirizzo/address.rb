05f8e903d9b49ed29e69e69b0cd2488b3b028a2f
Dave Worth <dave@bignerdranch.com>
2013-05-04 23:56:39 -0400

    extract #assign_text_to_address to Parser

--- lib/indirizzo/address.rb
+++ lib/indirizzo/address.rb
@@ -39,57 +39,7 @@ module Indirizzo
     end
 
     def assign_text_to_address(text)
-      if !text[:address].nil?
-        @text = clean text[:address]
-        parse
-      else
-        @street = []
-        @prenum = text[:prenum]
-        @sufnum = text[:sufnum]
-        if !text[:street].nil?
-          @street = text[:street].scan(Match[:street])
-        end
-        @number = ""
-        if !@street.nil?
-          if text[:number].nil?
-            @street.map! { |single_street|
-              single_street.downcase!
-              @number = single_street.scan(Match[:number])[0].reject{|n| n.nil? || n.empty?}.first.to_s
-              single_street.sub! @number, ""
-              single_street.sub! /^\s*,?\s*/o, ""
-            }
-          else
-            @number = text[:number].to_s
-          end
-          @street = expand_streets(@street) if @options[:expand_streets]
-          street_parts
-        end
-        @city = []
-        if !text[:city].nil?
-          @city.push(text[:city])
-          @text = text[:city].to_s
-        else
-          @city.push("")
-        end
-        if !text[:region].nil?
-          # @state = []
-          @state = text[:region]
-          if @state.length > 2
-            # full_state = @state.strip # special case: New York
-            @state = State[@state]
-          end
-        elsif !text[:state].nil?
-          @state = text[:state]
-        elsif !text[:country].nil?
-          @state = text[:country]
-        end
-
-        @zip = text[:postal_code]
-        @plus4 = text[:plus4]
-        if !@zip
-          @zip = @plus4 = ""
-        end
-      end
+      @text, @city, @street, @number, @prenum, @sufnum, @full_state, @state, @zip, @plus4, @country = Parser.extract_data_from_hash(text, @options)
     end
 
     def expand_numbers (string)
