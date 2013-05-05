8061535fa401df4cc21287d1ec84783dacfee17f
Dave Worth <dave@bignerdranch.com>
2013-05-04 23:15:50 -0400

    extract Match regexps

--- lib/indirizzo/address.rb
+++ lib/indirizzo/address.rb
@@ -1,19 +1,8 @@
 require 'indirizzo/constants'
 require 'indirizzo/number_helper'
+require 'indirizzo/match'
 
 module Indirizzo
-  # Defines the matching of parsed address tokens.
-  Match = {
-    # FIXME: shouldn't have to anchor :number and :zip at start/end
-    :number   => /^(\d+\W|[a-z]+)?(\d+)([a-z]?)\b/io,
-    :street   => /(?:\b(?:\d+\w*|[a-z'-]+)\s*)+/io,
-    :city     => /(?:\b[a-z][a-z'-]+\s*)+/io,
-    :state    => State.regexp,
-    :zip      => /\b(\d{5})(?:-(\d{4}))?\b/o,
-    :at       => /\s(at|@|and|&)\s/io,
-    :po_box => /\b[P|p]*(OST|ost)*\.*\s*[O|o|0]*(ffice|FFICE)*\.*\s*[B|b][O|o|0][X|x]\b/
-  }
-
   # The Address class takes a US street address or place name and
   # constructs a list of possible structured parses of the address
   # string.
